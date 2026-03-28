7d7c115147aa8bf8dc945ceaeee76888d1a4607b
hamzauys <hamza.uysal99@hotmail.com>
2026-03-28 20:26:24 +0300

    Required files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
+}
+
+export default App;
